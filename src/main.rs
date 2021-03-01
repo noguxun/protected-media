47b4d7f63d9cdce986175e78a0650f89a9f098d2
Xun Gu <noguxun@gmaifl.com>
2021-03-01 16:43:32 +0900

    remove comments

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,9 @@ use fastly::{mime, Error, Request, Response};
 use hmac_sha256::HMAC;
 use std::collections::HashMap;
 
-/// The name of a backend server associated with this service.
-///
-/// This should be changed to match the name of your own backend. See the the `Hosts` section of
-/// the Fastly WASM service UI for more information.
 const BACKEND_NAME: &str = "noguxun.github.io";
 const SECRET_KEY: &str = "this_is_your_secret_key";
 
-/// The entry point for your application.
-///
-/// This function is triggered when your service receives a client request. It could be used to
-/// route based on the request properties (such as method or path), send the request to a backend,
-/// make completely new requests, and/or generate synthetic responses.
-///
-/// If `main` returns an error, a 500 error response will be delivered to the client.
 #[fastly::main]
 fn main(mut req: Request) -> Result<Response, Error> {
     // Make any desired changes to the client request.
